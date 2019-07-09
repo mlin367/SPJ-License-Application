be5ddd48887c7c0a548cf4557d8b969ef90a8eb6
Matthew Lin <mlin367@yahoo.com>
2019-07-09 14:48:52 -0700

    create the mask function for customer name

--- mainFunctions/generateKey.js
+++ mainFunctions/generateKey.js
@@ -12,9 +12,33 @@ const parseDate = date => {
 const maskForAllocation = dateString => {
   let maskedString = '';
   for (let char of dateString) {
-    if (char === '0') maskedString += 'T';
+    if (char === '0') maskedString += 'Y';
     else if (char === '9') maskedString += 'X';
     else maskedString += parseInt(char) + 1;
+  }
+  return maskedString;
+}
+
+const maskForExpiry = dateString => {
+  let maskedString = '';
+  for (let char of dateString) {
+    if (char === '0') maskedString += 'Z';
+    else if (char === '9') maskedString += 'Y';
+    else maskedString += parseInt(char) + 2;
+  }
+  return maskedString;
+}
+
+const maskForCustomer = string => {
+  let maskedString = '';
+  for (let char of string) {
+    let charcode = char.charCodeAt();
+    if (charcode >= 88) {
+      charcode -= 25;
+    } else {
+      charcode += 3;
+    }
+    maskedString += String.fromCharCode(charcode);
   }
   return maskedString;
 }
